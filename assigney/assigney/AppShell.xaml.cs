d292677f940e9582dbeac9ae52e9bf438b31bf24
Sharizan Redzuan <ijanzz5621@gmail.com>
2022-02-23 10:42:12 +0800

    Add project files.

---
+++ assigney/assigney/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using assigney.ViewModels;
+using assigney.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace assigney
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ assigney/assigney/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using assigney.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace assigney.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
